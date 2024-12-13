21fbd080855d89b66d40bca659dd82fdcad8427e
lvingonzales <lvingonzales15@gmail.com>
2024-12-13 16:43:19 -0500

    Layout Main Project Page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import "./style-sidebar.css";
+import "./style-main.css";
 import { SidebarDisplay } from "./sidebar.js";
 export const containerDiv = document.querySelector(".container");
 
